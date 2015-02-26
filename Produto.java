cf96031873637bfbf979da2d5fb43f5f54f4aaee
calsUFAM <cals@icomp.ufam.edu.br>
2015-02-26 01:33:34 -0400

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,12 @@
+public class Produto {
+	String  id,categoria,titulo,descricao,img,preco;
+	
+	Produto(String i,String c,String t,String d,String im,String p){
+		id=i;
+		categoria=c;
+		titulo=t;
+		descricao=d;
+		img=im;
+		preco=p;
+	}
+}
